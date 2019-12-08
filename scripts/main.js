12fd7744362493f08a39446399975a6dd1fcd002
Morgan <morganlewiswalsh.mlw@gmail.com>
2019-12-08 19:40:26 +0000

    *lots of changes
    Added 'Nunito' as font on every element
    Removed unnecessary button on navbar
    Disabled contact button
    Added projects page
    Added carousel style content slider to index
    Image screenshots used on social media

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,42 @@
 let mainImage = document.querySelector("#mainImage")
+let contentList = document.getElementsByClassName("content-item")
+let previous = document.getElementById("goBack")
+let next = document.getElementById("goForward")
+let contentIndex = 0
 
 mainImage.addEventListener("click", function(){
     this.classList.toggle("main-image")
     this.classList.toggle("full-size-image")
 })
+
+// content slider
+previous.addEventListener("click", function(){
+    if(contentIndex > 0){
+        contentIndex = contentIndex - 1
+        removeActiveClass()
+        addActiveClass()
+        console.log(contentIndex)
+    }
+})
+
+next.addEventListener("click", function(){
+    if(contentIndex + 1 < contentList.length){
+        contentIndex = contentIndex + 1
+        removeActiveClass()
+        addActiveClass()
+        console.log(contentIndex)
+    }
+})
+
+function removeActiveClass(){
+    for(let i = 0; i < contentList.length; i++){
+        contentList[i].classList.remove("active")
+        contentList[i].classList.add("d-none")
+    }
+}
+
+function addActiveClass(){
+    // adds active class to current item
+    contentList[contentIndex].classList.add("active")
+    contentList[contentIndex].classList.remove("d-none")
+}
